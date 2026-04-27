93f9b9709dca7a7134f647fd7e87d078724cb5f0
elmbondo <fidelmahmbondo@gmail.com.noreply.github.com>
2026-04-28 00:03:10 +0300

    Add Google OAuth authentication

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+google_bp = make_google_blueprint(
+    client_id=os.environ.get("GOOGLE_CLIENT_ID"),
+    client_secret=os.environ.get("GOOGLE_CLIENT_SECRET"),
+    redirect_to="dashboard",
+    scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
+)
+app.register_blueprint(google_bp, url_prefix="/auth")
 
 @app.route('/')
 def index():
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if not google.authorized:
+        return redirect(url_for('index'))
+    user_info = google.get("/oauth2/v2/userinfo")
+    user = user_info.json()
+    return f"Welcome {user['name']}!"
 
 if __name__ == '__main__':
     app.run(debug=True)
